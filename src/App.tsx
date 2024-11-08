e58a9ddd5ba6aedb23508aa70a9a68187ba2fdcc
keven-bardales <keven.bardales@gmail.com>
2024-11-07 22:45:51 -0600

    feat: testing tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import "./App.css";
+import { Button } from "primereact/button";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button icon="pi pi-plus" className="mr-2" label="Increment"></Button>
+    </div>
+  );
 }
